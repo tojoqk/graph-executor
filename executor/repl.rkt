aa94ac73f9350cf8fcf78dd72a23d2ae72ba383f
Masaya Tojo <masaya@tojo.tokyo>
2026-06-24 23:18:08 +0900

    Fix REPL runner to handle missing graphs as terminated

--- executor/repl.rkt
+++ executor/repl.rkt
@@ -25,21 +25,24 @@
 
 (: repl-run (All (T S) (-> (Listof (Graph T S)) S (Node T S) (values S (Node T S)))))
 (define (repl-run gs st n)
-  (let ([g (find-graph gs (node-graph-id n))])
-    (displayln (format "--- Current Node: ~a (Graph: ~a) ---"
-                       (node-name n)
-                       (graph-name g)))
-    (let ([ne (next-edges gs st n)])
-      (case (car ne)
-        [(terminated)
-         (displayln ">> Terminated")
-         (values st n)]
-        [(auto)
-         (let ([chosen-edge (auto-choose ne)])
-           (displayln (format ">> [Auto] ~a" (edge-name chosen-edge)))
-           (let-values ([(next-st next-node) (step st chosen-edge)])
-             (repl-run gs next-st next-node)))]
-        [(choose)
-         (let ([chosen-edge (repl-choose ne)])
-           (let-values ([(next-st next-node) (step st chosen-edge)])
-             (repl-run gs next-st next-node)))]))))
+  (define (terminate)
+    (displayln ">> Terminated")
+    (values st n))
+  (cond [(find-graph gs (node-graph-id n))
+         => (lambda ([g : (Graph T S)])
+              (displayln (format "--- Current Node: ~a (Graph: ~a) ---"
+                                 (node-name n)
+                                 (graph-name g)))
+              (let ([ne (next-edges gs st n)])
+                (case (car ne)
+                  [(terminated) (terminate)]
+                  [(auto)
+                   (let ([chosen-edge (auto-choose ne)])
+                     (displayln (format ">> [Auto] ~a" (edge-name chosen-edge)))
+                     (let-values ([(next-st next-node) (step st chosen-edge)])
+                       (repl-run gs next-st next-node)))]
+                  [(choose)
+                   (let ([chosen-edge (repl-choose ne)])
+                     (let-values ([(next-st next-node) (step st chosen-edge)])
+                       (repl-run gs next-st next-node)))])))]
+        [else (terminate)]))
