6831c7713ee98b04a26e003c4e017271f7b5e240
Masaya Tojo <masaya@tojo.tokyo>
2026-06-28 18:58:52 +0900

    Change prompt attributes from keyword to optional  argument

--- prompt.rkt
+++ prompt.rkt
@@ -3,37 +3,38 @@
 (provide Prompt Prompt-Operator Prompt-Value current-prompt prompt)
 
 (define-type (Prompt A)
-  (case-> (-> String (List 'const
-                           (-> Any Boolean : #:+ A)
-                           (∩ A Prompt-Value))
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              (∩ A Prompt-Value))
-          (-> String (List 'choose
-                           (-> Any Boolean : #:+ A)
-                           (Listof (U (∩ A String)
-                                      (List (∩ A String) String))))
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              (∩ String A))
-          (-> String (List 'string)
-              [#:attributes (Immutable-HashTable Symbol Any)]String)
-          (-> String (List 'integer)
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              Integer)
-          (-> String (List 'natural)
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              Natural)
-          (-> String (List 'positive)
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              Positive-Integer)
-          (-> String (List 'range 'from Natural 'to Natural)
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              Natural)
-          (-> String (List 'range 'from Integer 'to Integer)
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              Integer)
-          (-> String (List 'random Positive-Integer)
-              [#:attributes (Immutable-HashTable Symbol Any)]
-              Natural)))
+  (case-> (->* (String (List 'const
+                             (-> Any Boolean : #:+ A)
+                             (∩ A Prompt-Value)))
+               ((Immutable-HashTable Symbol Any))
+               (∩ A Prompt-Value))
+          (->* (String (List 'choose
+                             (-> Any Boolean : #:+ A)
+                             (Listof (U (∩ A String)
+                                        (List (∩ A String) String)))))
+               ((Immutable-HashTable Symbol Any))
+               (∩ String A))
+          (->* (String (List 'string))
+               ((Immutable-HashTable Symbol Any))
+               String)
+          (->* (String (List 'integer))
+               ((Immutable-HashTable Symbol Any))
+               Integer)
+          (->* (String (List 'natural))
+               ((Immutable-HashTable Symbol Any))
+               Natural)
+          (->* (String (List 'positive))
+               ((Immutable-HashTable Symbol Any))
+               Positive-Integer)
+          (->* (String (List 'range 'from Natural 'to Natural))
+               ((Immutable-HashTable Symbol Any))
+               Natural)
+          (->* (String (List 'range 'from Integer 'to Integer))
+               ((Immutable-HashTable Symbol Any))
+               Integer)
+          (->* (String (List 'random Positive-Integer))
+               ((Immutable-HashTable Symbol Any))
+               Natural)))
 
 (define-type (Prompt-Operator A)
   (U (List 'choose
@@ -54,7 +55,7 @@
 (define current-prompt (make-parameter #f))
 
 (: prompt (All (A) (Prompt A)))
-(define (prompt title op #:attributes [attrs (hash)])
+(define (prompt title op [_ (hash)])
   (cond [(current-prompt) => (lambda ([p : (Prompt Any)])
                                (let ([value (p title op)])
                                  (case (car op)
