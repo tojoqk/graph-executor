c7734f98983819ded5fe6e587a4f6027cbcd3e9f
Masaya Tojo <masaya@tojo.tokyo>
2026-07-08 00:38:54 +0900

    Refine public API boundaries in main.rkt

--- main.rkt
+++ main.rkt
@@ -2,6 +2,16 @@
 
 (require "graph.rkt")
 (require "executor.rkt")
+(require "prompt.rkt")
+(require "message.rkt")
+(require "history.rkt")
 
-(provide (all-from-out "graph.rkt"
-                       "executor.rkt"))
+(provide current-node-prompt
+         Node node-maker Edge Bridge make-bridge make-edge
+         OpenGraph Graph make-open-graph make-graph
+         replay
+         current-auto-conflict-policy current-single-choose-policy
+         current-node? current-edge?
+         History Journal history->journal
+         prompt
+         message)
