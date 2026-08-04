d928fd105d16728349887a6d338ae9969c1f98dd
Masaya Tojo <masaya@tojo.tokyo>
2026-08-05 03:06:30 +0900

    Decouple consumer from amb and require explicit Nothing-returning fail handler

--- effect/consumer.rkt
+++ effect/consumer.rkt
@@ -2,29 +2,25 @@
 
 (provide make-consumer)
 
-(require "amb.rkt")
 (require "state.rkt")
 
-(: make-consumer (All (A B C) (-> (Values (-> (-> A) (Listof B) (-> C) (U A C))
-                                          (-> B)))))
+(: make-consumer (All (A B) (-> (Values (-> (-> A) (Listof B) A)
+                                        (-> (-> Nothing) B)))))
 (define (make-consumer)
-  (define-values (call-with-amb-prompt amb) ((inst make-amb A C)))
   (define-values (call-with-state-prompt get set) ((inst make-state A (Listof B))))
 
-  (: call-with-consumer-prompt (-> (-> A) (Listof B) (-> C) (U A C)))
-  (define (call-with-consumer-prompt proc lst fail)
-    (call-with-amb-prompt
-     (lambda ()
-       (define-values (x _)
-         (call-with-state-prompt proc lst))
-       x)
-     fail))
+  (: call-with-consumer-prompt (-> (-> A) (Listof B) A))
+  (define (call-with-consumer-prompt proc lst)
+    (define-values (x _)
+      (call-with-state-prompt proc lst))
+    x)
 
-  (: consume (-> B))
-  (define (consume)
+  (: consume (-> (-> Nothing) B))
+  (define (consume fail)
     (let ([lst : (Listof B) (get)])
-      (when (null? lst) (amb))
-      (set (cdr lst))
-      (car lst)))
+      (if (null? lst)
+          (fail)
+          (begin (set (cdr lst))
+                 (car lst)))))
 
   (values call-with-consumer-prompt consume))
