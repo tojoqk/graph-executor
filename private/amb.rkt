e487abccfb8b65528b06d725856b278f12532eb6
Masaya Tojo <masaya@tojo.tokyo>
2026-08-04 20:10:25 +0900

    Simplify amb implementation and decouple from emitter

--- private/amb.rkt
+++ private/amb.rkt
@@ -1,52 +1,40 @@
 #lang typed/racket
 
-(require "./emitter.rkt")
-
 (provide make-amb)
 
-(: make-amb (All (A B) (-> (Values (-> (-> (Values (Option A) B))
-                                       (-> B)
-                                       (Values (Option A) B))
-                                   (-> (Listof (-> A)) A)))))
+(: make-amb (All (A B) (-> (Values (case-> (-> (-> A) (Option A))
+                                           (-> (-> A) (-> B) (U A B)))
+                                   (-> (-> A) * A)))))
 (define (make-amb)
-  (: amb-tag (Prompt-Tagof (Option A)
-                           (-> (-> (Option A))
-                               (Option A))))
+  (: amb-tag (Prompt-Tagof (Option (List A))
+                           (-> (-> (Option (List A))) (Option (List A)))))
   (define amb-tag (make-continuation-prompt-tag 'amb))
 
-  (define-values (call-with-emitter-prompt emit)
-    ((inst make-emitter (Option A) B)))
+  (: call-with-amb-prompt (case-> (-> (-> A) (Option A))
+                                  (-> (-> A) (-> B) (U A B))))
+  (define call-with-amb-prompt
+    (case-lambda
+      [(proc)
+       (cond [(call-with-continuation-prompt (lambda () (list (proc))) amb-tag) => car]
+             [else #f])]
+      [(proc fail)
+       (cond [(call-with-continuation-prompt (lambda () (list (proc))) amb-tag) => car]
+             [else (fail)])]))
 
-  (: call-with-amb-prompt (-> (-> (Values (Option A) B))
-                              (-> B)
-                              (Values (Option A) B)))
-  (define (call-with-amb-prompt proc default)
-    (define-values (x ctx)
-      (call-with-emitter-prompt
-       (lambda () : (Option A)
-         (call-with-continuation-prompt
-          (lambda ()
-            (define-values (x ctx) (proc))
-            (emit ctx)
-            x)
-          amb-tag))))
-    (if (null? ctx)
-        (values x (default))
-        (values x (first ctx))))
-
-  (: amb (-> (Listof (-> A)) A))
-  (define (amb xs)
-    (call-with-composable-continuation
-     (lambda ([k : (-> A (Option A))])
-       (let ([k (lambda ([n : A])
-                  (call-with-continuation-prompt (lambda () (k n)) amb-tag))])
-         (abort-current-continuation
-          amb-tag
-          (lambda () : (Option A)
-            (let loop ([xs xs])
-              (cond [(null? xs) #f]
-                    [(k ((car xs))) => identity]
-                    [else (loop (cdr xs))]))))))
-     amb-tag))
+  (: amb (-> (-> A) * A))
+  (define (amb . xs)
+    (car
+     (call-with-composable-continuation
+      (lambda ([k : (-> (List A) (Option (List A)))])
+        (let ([k (lambda ([x : (List A)])
+                   (call-with-continuation-prompt (lambda () (k x)) amb-tag))])
+          (abort-current-continuation
+           amb-tag
+           (lambda () : (Option (List A))
+             (let loop ([xs xs])
+               (cond [(null? xs) #f]
+                     [(k (list ((car xs)))) => identity]
+                     [else (loop (cdr xs))]))))))
+      amb-tag)))
 
   (values call-with-amb-prompt amb))
