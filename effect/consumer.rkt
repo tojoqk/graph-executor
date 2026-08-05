476076af2aa7fd4e9a88315acc13d98c5fc313f0
Masaya Tojo <masaya@tojo.tokyo>
2026-08-05 21:30:48 +0900

    Return remaining list from call-with-consumer-prompt

--- effect/consumer.rkt
+++ effect/consumer.rkt
@@ -4,16 +4,14 @@
 
 (require "state.rkt")
 
-(: make-consumer (All (A B) (-> (Values (-> (-> A) (Listof B) A)
+(: make-consumer (All (A B) (-> (Values (-> (-> A) (Listof B) (Values A (Listof B)))
                                         (-> (-> Nothing) B)))))
 (define (make-consumer)
   (define-values (call-with-state-prompt get set) ((inst make-state A (Listof B))))
 
-  (: call-with-consumer-prompt (-> (-> A) (Listof B) A))
+  (: call-with-consumer-prompt (-> (-> A) (Listof B) (Values A (Listof B))))
   (define (call-with-consumer-prompt proc lst)
-    (define-values (x _)
-      (call-with-state-prompt proc lst))
-    x)
+    (call-with-state-prompt proc lst))
 
   (: consume (-> (-> Nothing) B))
   (define (consume fail)
