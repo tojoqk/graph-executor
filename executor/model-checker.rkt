080b4dbdade1cf9679ee31de171686132f712a65
Masaya Tojo <masaya@tojo.tokyo>
2026-08-07 00:39:41 +0900

    Add model checker executor and prompt modules

---
+++ executor/model-checker.rkt
@@ -0,0 +1,126 @@
+#lang typed/racket
+
+(require "../graph.rkt")
+(require "../prompt.rkt")
+(require "../message.rkt")
+(require "../prompt/model-checker.rkt")
+(require "../executor.rkt")
+(require "../journal.rkt")
+(require "../effect/amb.rkt")
+(require "../effect/emitter.rkt")
+
+(provide model-checker-run
+         current-model-checker-counterexample-display
+         current-model-checker-trace-display)
+
+(: current-model-checker-counterexample-display (Parameterof (U 'show 'hide)))
+(define current-model-checker-counterexample-display (make-parameter 'show))
+
+(: current-model-checker-counterexample-display? (-> Boolean))
+(define (current-model-checker-counterexample-display?)
+  (case (current-model-checker-counterexample-display)
+    [(show) #t]
+    [(hide) #f]))
+
+(: current-model-checker-trace-display (Parameterof (U 'show 'hide)))
+(define current-model-checker-trace-display (make-parameter 'hide))
+
+(: current-model-checker-trace-display? (-> Boolean))
+(define (current-model-checker-trace-display?)
+  (case (current-model-checker-trace-display)
+    [(show) #t]
+    [(hide) #f]))
+
+(define-type Next-Mode (U 'terminated 'auto 'choose))
+
+(: model-checker-run (All (S) (case-> (-> (Listof (Graph S)) (Node S) S
+                                          #:mode 'first
+                                          #:invariant (-> Next-Mode Symbol S Any)
+                                          [#:max-depth (Option Natural)]
+                                          (Option Journal))
+                                      (-> (Listof (Graph S)) (Node S) S
+                                          #:mode 'all
+                                          #:invariant (-> Next-Mode Symbol S Any)
+                                          [#:max-depth (Option Natural)]
+                                          (Listof Journal)))))
+(define (model-checker-run gs entry initial-state
+                           #:mode mode
+                           #:invariant invariant
+                           #:max-depth [max-depth #f])
+  (define-values (call-with-journal-emitter journal-emit)
+    ((inst make-emitter Journal 'done)))
+  (define-values (call-with-prompt-value-emitter prompt-value-emit)
+    ((inst make-emitter (Pairof Prompt-Value Prompt-Attributes) S)))
+  (define-values (call-with-amb amb amb-fail)
+    ((inst make-amb Prompt-Value)))
+  (: prompt Prompt-Implementation)
+  (define result
+    (car
+     (call-with-journal-emitter
+      (thunk
+       (let/ec return : 'done
+         (call-with-amb
+          (thunk
+           (let loop : 'done ([n entry]
+                              [st initial-state]
+                              [j : Journal '()]
+                              [depth 0]
+                              [seen : (Setof (Pairof Symbol S)) (set)])
+             (define seen-key `(,(node-id n) . ,st))
+             (when (set-member? seen seen-key)
+               (amb-fail))
+             (define ne (next-edges gs st n))
+             (unless (invariant (car ne) (node-type n) st)
+               (journal-emit j)
+               (when (current-model-checker-counterexample-display?)
+                 (printf "Counterexample: ~s\n" j))
+               (case mode
+                 [(first) (return 'done)]
+                 [(all) (amb-fail)]))
+             (case (car ne)
+               [(terminated) 'done]
+               [(auto choose)
+                (define-values (name _)
+                  ((model-checker-prompt amb) "choose"
+                                              `(choose ,(map (inst edge-name S) (second ne)))))
+                (define chosen-edge (find-edge (second ne) name))
+                (when (current-model-checker-trace-display?)
+                  (printf "Current Edge: ~a (Graph: ~a)\n" (edge-name chosen-edge) (node-graph-name n)))
+                (match-define (cons ps next-st)
+                  (call-with-prompt-value-emitter
+                   (thunk (step st chosen-edge amb prompt-value-emit))))
+                (if (and max-depth (= max-depth depth))
+                    (amb-fail)
+                    (loop (edge-to chosen-edge)
+                          next-st
+                          (cons (choose-journal-entry (edge-name chosen-edge) '() ps) j)
+                          (add1 depth)
+                          (set-add seen seen-key)))])))
+          (thunk 'done)))))))
+  (case mode
+    [(first) (if (null? result) #f (car result))]
+    [(all) result]))
+
+(: step (All (S) (-> S
+                     (Edge S)
+                     (-> (-> Prompt-Value) * Prompt-Value)
+                     (-> (Pairof Prompt-Value Prompt-Attributes) Void)
+                     S)))
+(define (step st e amb emit)
+  (define (void-message val)
+    (void))
+  (parameterize ([current-prompt (prompt/log amb emit)]
+                 [current-message void-message])
+    ((node-trans (edge-to e))
+     (begin0 ((edge-trans e) st)
+       (when (current-model-checker-trace-display?)
+         (let ([n (edge-to e)])
+           (printf "Current Node: ~a (Graph: ~a)\n" (node-name n) (node-graph-name n))))))))
+
+(: prompt/log (All (S) (-> (-> (-> Prompt-Value) * Prompt-Value)
+                           (-> (Pairof Prompt-Value Prompt-Attributes) Void)
+                           Prompt-Implementation)))
+(define ((prompt/log amb emit) title op)
+  (define-values (val attrs) ((model-checker-prompt amb) title op))
+  (emit (cons val attrs))
+  (values val attrs))
---
+++ prompt/model-checker.rkt
@@ -0,0 +1,80 @@
+#lang typed/racket
+
+(require "../prompt.rkt")
+
+(provide model-checker-prompt
+         current-model-checker-string-value
+         current-model-checker-integer-value
+         current-model-checker-natural-value
+         current-model-checker-positive-integer-value)
+
+(: current-model-checker-string-value (Parameterof String))
+(define current-model-checker-string-value (make-parameter "test"))
+
+(: current-model-checker-integer-value (Parameterof Integer))
+(define current-model-checker-integer-value (make-parameter -1))
+
+(: current-model-checker-natural-value (Parameterof Natural))
+(define current-model-checker-natural-value (make-parameter 0))
+
+(: current-model-checker-positive-integer-value (Parameterof Positive-Integer))
+(define current-model-checker-positive-integer-value (make-parameter 1))
+
+(: model-checker-prompt (-> (-> (-> Prompt-Value) * Prompt-Value) Prompt-Implementation))
+(define ((model-checker-prompt amb) title op)
+  (case (car op)
+    [(choose) (model-checker-choose amb title op)]
+    [(integer) (values (current-model-checker-integer-value) '())]
+    [(natural) (values (current-model-checker-natural-value) '())]
+    [(positive-integer) (values (current-model-checker-positive-integer-value) '())]
+    [(string) (values (current-model-checker-string-value) '())]
+    [(range) (model-checker-range amb title op)]
+    [(random) (model-checker-random amb title op)]))
+
+(: model-checker-choose (-> (-> (-> Prompt-Value) * Prompt-Value)
+                            String
+                            (U (List 'choose Procedure (Listof String))
+                               (List 'choose (Listof String)))
+                            (Values String Prompt-Attributes)))
+(define (model-checker-choose amb _title op)
+  (let* ([choices (if (procedure? (second op))
+                      (third op)
+                      (second op))]
+         [value (let loop : Prompt-Value ([choices choices])
+                  (if (null? choices)
+                      (amb)
+                      (amb (thunk (car choices))
+                           (thunk (loop (cdr choices))))))])
+    (values (assert value string?) '())))
+
+(: model-checker-range (case-> (-> (-> (-> Prompt-Value) * Prompt-Value)
+                             String (List 'range Positive-Integer Positive-Integer) (Values Positive-Integer Prompt-Attributes))
+                         (-> (-> (-> Prompt-Value) * Prompt-Value)
+                             String (List 'range Natural Natural) (Values Natural Prompt-Attributes))
+                         (-> (-> (-> Prompt-Value) * Prompt-Value)
+                             String (List 'range Integer Integer) (Values Integer Prompt-Attributes))))
+(define (model-checker-range amb title op)
+  (let* ([from (second op)]
+         [to : Integer (third op)])
+    (unless (<= from to)
+      (error 'model-checker-prompt "invalid range ~a...~a" from to))
+    (let ([value (let loop : Prompt-Value ([i from])
+                   (if (< to i)
+                       (amb)
+                       (amb (thunk i)
+                            (thunk (loop (add1 i))))))])
+      (if (and (exact? value) (integer? value)
+               (<= from value) (<= value to))
+          (values value '())
+          (error 'model-checker-prompt "invalid range value ~a" value)))))
+
+(: model-checker-random (-> (-> (-> Prompt-Value) * Prompt-Value) String (List 'random Positive-Integer) (Values Natural Prompt-Attributes)))
+(define (model-checker-random amb title op)
+  (let ([n (second op)])
+    (values (assert (let loop : Prompt-Value ([i 0])
+                      (if (= i n)
+                          (amb)
+                          (amb (thunk i)
+                               (thunk (loop (add1 i))))))
+                    natural?)
+            '())))
