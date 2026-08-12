5a0cc3851da9401e3f5b34e079e721a054fa92c5
Masaya Tojo <masaya@tojo.tokyo>
2026-08-13 01:23:18 +0900

    Add triangle graph model for livelock detection tests

--- examples/unreachble-terminal-graph.rkt
+++ examples/unreachble-terminal-graph.rkt
@@ -15,12 +15,37 @@
             #:edges (list (g-edge "Id" #:from n #:to n)))
    n))
 
+(: triangle-graph (-> String (Values (Graph Null) (Node Null))))
+(define (triangle-graph graph-name)
+  (define g-node (inst (node graph-name) Null 'node))
+  (define g-edge (inst dot-edge Null))
+  (define g-graph (inst graph Null))
+
+  (define a (g-node "A" #:type 'node))
+  (define b (g-node "B" #:type 'node))
+  (define c (g-node "C" #:type 'node))
+
+  (values
+   (g-graph graph-name
+            #:edges (list (g-edge "A->B" #:from a #:to b)
+                          (g-edge "B->C" #:from b #:to c)
+                          (g-edge "C->A" #:from c #:to a)))
+   a))
+
+
 (module+ model
-  (provide make-id-model)
+  (provide make-id-model
+           make-triangle-model)
 
   (: make-id-model (-> (Model Null)))
   (define (make-id-model)
     (define-values (graph node-init) (id-graph "ID"))
+    (define graphs (list graph))
+    (model graphs node-init '()))
+
+  (: make-triangle-model (-> (Model Null)))
+  (define (make-triangle-model)
+    (define-values (graph node-init) (triangle-graph "TRIANGLE"))
     (define graphs (list graph))
     (model graphs node-init '())))
 
@@ -28,5 +53,8 @@
   (require typed/rackunit)
   (require (submod ".." model))
 
-  (define m (make-id-model))
-  (check-equal? (find-livelock m) '((choose ("Id")))))
+  (define id-m (make-id-model))
+  (check-equal? (find-livelock id-m) '((choose ("Id"))))
+
+  (define tri-m (make-triangle-model))
+  (check-equal? (find-livelock tri-m) '((choose ("C->A")) (choose ("B->C")) (choose ("A->B")))))
