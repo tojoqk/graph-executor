6107819be02424eb751e4ca38468c4c2745e2741
Masaya Tojo <masaya@tojo.tokyo>
2026-08-18 09:26:52 +0900

    Enhance vending-graph example to demonstrate bidirectional graph composition

--- examples/vending-graph.rkt
+++ examples/vending-graph.rkt
@@ -7,41 +7,41 @@
   #:type-name Vending-State
   #:transparent)
 
-(: insert-money (-> Vending-State Vending-State))
-(define (insert-money st)
-  (let ([amount (prompt "How much?" `(range 1 ,(v-state-wallet st)))])
-    (struct-copy v-state st
-                 [wallet (- (v-state-wallet st) amount)]
-                 [inserted (+ (v-state-inserted st) amount)])))
-
-(: purchase (-> Integer (-> Vending-State Vending-State)))
-(define ((purchase amount) st)
-  (struct-copy v-state st
-               [inserted (- (v-state-inserted st) amount)]))
-
-(: reset-money (-> Vending-State Vending-State))
-(define (reset-money st)
-  (struct-copy v-state st
-               [wallet (+ (v-state-wallet st) (v-state-inserted st))]
-               [inserted 0]))
-
-(: price-met? (-> Integer (-> Vending-State Boolean)))
-(define ((price-met? price) st)
-  (>= (v-state-inserted st) price))
-
-(: can-insert? (-> Integer (-> Vending-State Boolean)))
-(define ((can-insert? price) st)
-  (<= price (v-state-wallet st)))
-
-(: inserted? (-> Vending-State Boolean))
-(define (inserted? st)
-  (< 0 (v-state-inserted st)))
-
 (: vending-graph (-> String
                      (Values (-> AnyNode (Code (-> Vending-State Any))
                                  (OpenGraph Vending-State))
                              (Node Vending-State))))
 (define (vending-graph g)
+  (: insert-money (-> Vending-State Vending-State))
+  (define (insert-money st)
+    (let ([amount (prompt "How much?" `(range 1 ,(v-state-wallet st)))])
+      (struct-copy v-state st
+                   [wallet (- (v-state-wallet st) amount)]
+                   [inserted (+ (v-state-inserted st) amount)])))
+
+  (: purchase (-> Integer (-> Vending-State Vending-State)))
+  (define ((purchase amount) st)
+    (struct-copy v-state st
+                 [inserted (- (v-state-inserted st) amount)]))
+
+  (: reset-money (-> Vending-State Vending-State))
+  (define (reset-money st)
+    (struct-copy v-state st
+                 [wallet (+ (v-state-wallet st) (v-state-inserted st))]
+                 [inserted 0]))
+
+  (: price-met? (-> Integer (-> Vending-State Boolean)))
+  (define ((price-met? price) st)
+    (>= (v-state-inserted st) price))
+
+  (: can-insert? (-> Integer (-> Vending-State Boolean)))
+  (define ((can-insert? price) st)
+    (<= price (v-state-wallet st)))
+
+  (: inserted? (-> Vending-State Boolean))
+  (define (inserted? st)
+    (< 0 (v-state-inserted st)))
+
   (define v-node (inst (node g) Vending-State (U 'start 'normal)))
   (define v-edge (inst edge Vending-State))
   (define v-bridge (inst dot-bridge Vending-State))
@@ -82,47 +82,60 @@
                  #:dot-minlen 3))))
    idle))
 
-(struct terminal ([wallet : Integer])
-  #:type-name Terminal
+(struct street ([wallet : Integer])
+  #:type-name Street-State
   #:transparent)
 
-(: terminal-graph (-> String
-                      (Values (-> (OpenGraph Terminal))
-                              (Node Terminal))))
-(define (terminal-graph g)
-  (define t-node (inst (node g) Terminal (U 'entry 'terminal)))
-  (define t-graph (inst open-graph Terminal))
-  (define t-edge (inst edge Terminal))
-  (define entry (t-node "Terminal Entry" #:type 'entry))
-  (define terminal (t-node "Terminal" #:type 'terminal))
+(: street-graph (-> String
+                    (Values
+                     (-> AnyNode (Code (-> Street-State Any)) (OpenGraph Street-State))
+                     (Node Street-State))))
+(define (street-graph g)
+  (define s-node (inst (node g) Street-State (U 'street 'terminal)))
+  (define s-graph (inst open-graph Street-State))
+  (define s-edge (inst edge Street-State))
+  (define s-bridge (inst bridge Street-State))
+
+  (define on-street (s-node "Street Entry" #:type 'street))
+  (define bench (s-node "Bench (Rest)" #:type 'terminal))
 
   (values
-   (lambda ()
-     (t-graph g
+   (lambda (output output-edge)
+     (s-graph g
               #:edges
               (list
-               (t-edge "Terminate" #:mode 'auto #:from entry #:to terminal))))
-   entry))
+               (s-edge "Sit on Bench" #:mode 'choose #:from on-street #:to bench
+                       #:priority -1))
+              #:bridges
+              (list
+               (s-bridge "Go to Vending Machine" #:from on-street #:to output
+                         #:trans output-edge))))
+   on-street))
 
-(: vending-graph->terminal-graph (-> Vending-State Terminal))
-(define (vending-graph->terminal-graph x)
-  (terminal (v-state-wallet x)))
+(: vending-graph->street-graph (-> Vending-State Street-State))
+(define (vending-graph->street-graph x)
+  (street (v-state-wallet x)))
+
+(: street-graph->vending-graph (-> Street-State Vending-State))
+(define (street-graph->vending-graph x)
+  (v-state (street-wallet x) 0))
 
 (: wire (-> (Values (Listof AnyGraph) AnyNode)))
 (define (wire)
   (define v-any-graph (any-graph v-state?))
-  (define t-any-graph (any-graph terminal?))
-  (define t-any-node (any-node terminal?))
+  (define t-any-graph (any-graph street?))
+  (define t-any-node (any-node street?))
   (define v-any-node (any-node v-state?))
   (define-values (gen-t-graph t-entry)
-    (terminal-graph "Terminal"))
+    (street-graph "Street"))
   (define-values (gen-v-graph v-entry)
     (vending-graph "Vending Machine Model"))
 
   (values (list (v-any-graph (gen-v-graph (t-any-node t-entry)
-                                          (code vending-graph->terminal-graph)))
-                (t-any-graph (gen-t-graph)))
-          (v-any-node v-entry)))
+                                          (code vending-graph->street-graph)))
+                (t-any-graph (gen-t-graph (v-any-node v-entry)
+                                          (code street-graph->vending-graph))))
+          (t-any-node t-entry)))
 
 (module+ model
   (provide make-model)
@@ -132,7 +145,7 @@
     (model
      (thunk
       (define-values (graphs node-init) (wire))
-      (define state-init (v-state 300 0))
+      (define state-init (street 300))
       (values graphs node-init state-init)))))
 
 (module+ main
@@ -156,7 +169,7 @@
   (require typed/rackunit)
   (require (submod ".." model))
 
-  (: terminal-node? (All (S) (-> (Node S) Any)))
+  (: terminal-node? (-> (Node Any) Any))
   (define (terminal-node? n)
     (symbol=? (node-type n) 'terminal))
   
@@ -165,17 +178,20 @@
   (check-false (find-deadlock m terminal-node?))
   (check-false (find-false-terminal m terminal-node?))
   (check-false (find-counterexample m
-                                    (lambda (_n st)
-                                      (or (not (v-state? st))
-                                          (not (negative? (v-state-wallet st)))))))
+                                    (negate (lambda (_n st)
+                                              (and (v-state? st)
+                                                   (negative? (v-state-wallet st)))))))
 
-  (check-equal? (find-counterexample m (lambda (_n st)
-                                         (or (not (v-state? st))
-                                             (not (zero? (v-state-wallet st))))))
-                (append (make-list 299 '(choose ("Insert More") (1)))
-                        (list '(choose ("Insert Money") (1)))))
-  (check-equal? (find-counterexample m (lambda (_n st)
-                                         (or (not (v-state? st))
-                                             (not (zero? (v-state-wallet st)))))
-                                     #:bound 1)
-                '((choose ("Insert Money") (300)))))
+  (check-equal? (find-counterexample m (negate (lambda ([n : (Node Any)] st)
+                                                 (and (terminal-node? n)
+                                                      (street? st)
+                                                      (= (street-wallet st) 0)))))
+                `((choose ("Sit on Bench"))
+                  (choose ("Walk Away"))
+                  (auto ("Dispense Done (Just Zero)"))
+                  (choose ("Purchase Drink (150 Yen)"))
+                  (auto ("Dispense Done (Remaining Inserted)"))
+                  (choose ("Purchase Drink (150 Yen)"))
+                  ,@(make-list 299 '(choose ("Insert More") (1)))
+                  (list '(choose ("Insert Money") (1)))
+                  (choose ("Go to Vending Machine")))))
