271f13db4a5efe2490cc5aa0e40b7912fcbc05eb
Masaya Tojo <masaya@tojo.tokyo>
2026-08-21 00:37:43 +0900

    Generalize water-jug example with statically generated edges and drop dynamic version

--- examples/water-jug.rkt
+++ examples/water-jug.rkt
@@ -2,19 +2,45 @@
 
 (require graph-executor)
 
-(struct jug-state ([left : Integer]
-                   [right : Integer])
-  #:type-name Jug-State
-  #:transparent)
+(define-type Jug-State (Immutable-HashTable Positive-Integer Integer))
 
-(: jug-graph (-> String Positive-Integer Positive-Integer Positive-Integer
+(: caps->jug-state (-> (Listof Positive-Integer) Jug-State))
+(define (caps->jug-state caps)
+  (for/hash : (Immutable-HashTable Positive-Integer Integer) ([cap (in-list caps)])
+    (values cap 0)))
+
+(: dup (-> (Listof Positive-Integer) (Option Positive-Integer)))
+(define (dup np)
+  (cond [(null? np) #f]
+        [(member (car np) (cdr np)) => car]
+        [else (dup (cdr np))]))
+
+(: uniq (All (A) (-> (Listof A) (Listof A))))
+(define (uniq xs)
+  (cond [(null? xs) '()]
+        [(member (car xs) (cdr xs)) (uniq (cdr xs))]
+        [else (cons (car xs) (uniq (cdr xs)))]))
+
+(define-type Command-Type (U 'Fill 'Empty 'Pour))
+
+(define-predicate command-type? Command-Type)
+
+(: cap->symbol (-> Positive-Integer Symbol))
+(define (cap->symbol c) (string->symbol (format "~aG" c)))
+
+(: symbol->cap (-> Symbol Positive-Integer))
+(define (symbol->cap c)
+  (assert (string->number (car (string-split (symbol->string c) "G")))
+          exact-positive-integer?))
+
+(: jug-graph (-> String (Listof Positive-Integer) Natural
                  (Values (Graph Jug-State) (Node Jug-State))))
-(define (jug-graph g left-cap right-cap target)
-  (when (= left-cap right-cap)
-    (error 'jug-graph "must not be same caps (~a, ~a)" left-cap right-cap))
+(define (jug-graph g caps target)
+  (cond [(dup caps) => (lambda ([dup-cap : Positive-Integer])
+                         (error 'jug-graph "must not be same caps (~a, ~a)" dup-cap dup-cap))])
 
   (define j-node (inst (node g) Jug-State (U 'puzzle 'check 'terminal)))
-  (define j-edge (inst dot-edge Jug-State))
+  (define j-edge (inst edge Jug-State))
   (define j-graph (inst graph Jug-State))
 
   (: show-cleared (-> Jug-State Jug-State))
@@ -22,73 +48,52 @@
     (message (format "Congratulations! You made exactly ~a gallons!" target))
     st)
 
-  (: fill-left (-> Jug-State Jug-State))
-  (define (fill-left st)
-    (message (format "Filled the ~a-gallon jug." left-cap))
-    (struct-copy jug-state st [left left-cap]))
+  (: fill (-> Positive-Integer (-> Jug-State Jug-State)))
+  (define ((fill cap) st)
+    (message (format "Filled the ~a-gallon jug." cap))
+    (hash-set st cap cap))
 
-  (: fill-right (-> Jug-State Jug-State))
-  (define (fill-right st)
-    (message (format "Filled the ~a-gallon jug." right-cap))
-    (struct-copy jug-state st [right right-cap]))
+  (: empty (-> Positive-Integer (-> Jug-State Jug-State)))
+  (define ((empty cap) st)
+    (message (format "Emptied the ~a-gallon jug." cap))
+    (hash-set st cap 0))
 
-  (: empty-left (-> Jug-State Jug-State))
-  (define (empty-left st)
-    (message (format "Emptied the ~a-gallon jug." left-cap))
-    (struct-copy jug-state st [left 0]))
+  (: pour (-> Positive-Integer Positive-Integer (-> Jug-State Jug-State)))
+  (define ((pour src dst) st)
+    (let* ([amount (min (hash-ref st src) (- dst (hash-ref st dst)))]
+           [new-src (- (hash-ref st src) amount)]
+           [new-dst (+ (hash-ref st dst) amount)])
+      (message (format "Poured ~a gallons from ~aG to ~aG." amount src dst))
+      (hash-set (hash-set st src new-src) dst new-dst)))
 
-  (: empty-right (-> Jug-State Jug-State))
-  (define (empty-right st)
-    (message (format "Emptied the ~a-gallon jug." right-cap))
-    (struct-copy jug-state st [right 0]))
+  (: can-fill? (-> Positive-Integer (-> Jug-State Boolean)))
+  (define ((can-fill? cap) st) (< (hash-ref st cap) cap))
 
-  (: pour-left-to-right (-> Jug-State Jug-State))
-  (define (pour-left-to-right st)
-    (let* ([amount (min (jug-state-left st) (- right-cap (jug-state-right st)))]
-           [new-left (- (jug-state-left st) amount)]
-           [new-right (+ (jug-state-right st) amount)])
-      (message (format "Poured ~a gallons from ~aG to ~aG." amount left-cap right-cap))
-      (struct-copy jug-state st [left new-left] [right new-right])))
+  (: can-empty? (-> Positive-Integer (-> Jug-State Boolean)))
+  (define ((can-empty? cap) st) (> (hash-ref st cap) 0))
 
-  (: pour-right-to-left (-> Jug-State Jug-State))
-  (define (pour-right-to-left st)
-    (let* ([amount (min (jug-state-right st) (- left-cap (jug-state-left st)))]
-           [new-right (- (jug-state-right st) amount)]
-           [new-left (+ (jug-state-left st) amount)])
-      (message (format "Poured ~a gallons from ~aG to ~aG." amount right-cap left-cap))
-      (struct-copy jug-state st [right new-right] [left new-left])))
-
-  (: can-fill-right? (-> Jug-State Boolean))
-  (define (can-fill-right? st) (< (jug-state-right st) right-cap))
-
-  (: can-fill-left? (-> Jug-State Boolean))
-  (define (can-fill-left? st) (< (jug-state-left st) left-cap))
-
-  (: can-empty-right? (-> Jug-State Boolean))
-  (define (can-empty-right? st) (> (jug-state-right st) 0))
-
-  (: can-empty-left? (-> Jug-State Boolean))
-  (define (can-empty-left? st) (> (jug-state-left st) 0))
-
-  (: can-pour-right-to-left? (-> Jug-State Boolean))
-  (define (can-pour-right-to-left? st)
-    (and (> (jug-state-right st) 0) (< (jug-state-left st) left-cap)))
-
-  (: can-pour-left-to-right? (-> Jug-State Boolean))
-  (define (can-pour-left-to-right? st)
-    (and (> (jug-state-left st) 0) (< (jug-state-right st) right-cap)))
+  (: can-pour? (-> Positive-Integer Positive-Integer (-> Jug-State Boolean)))
+  (define ((can-pour? src dst) st)
+    (and (not (= src dst))
+         (< 0 (hash-ref st src))
+         (< (hash-ref st dst) dst)))
 
   (: is-cleared? (-> Jug-State Boolean))
   (define (is-cleared? st)
-    (or (= (jug-state-left st) target)
-        (= (jug-state-right st) target)))
+    (and (memf (lambda ([cap : Positive-Integer])
+                 (= (hash-ref st cap) target))
+               caps)
+         #t))
 
   (: prompt-playing (-> Jug-State String))
   (define (prompt-playing st)
-    (format "Goal: Make exactly ~aG\n  Current Status:\n    [ ~aG Jug: ~a/~a | ~aG Jug: ~a/~a ]\n  What will you do?"
+    (format "Goal: Make exactly ~aG\n  Current Status:\n~a\n  What will you do?"
             target
-            left-cap (jug-state-left st) left-cap
-            right-cap (jug-state-right st) right-cap))
+            (string-join (map (lambda ([cap : Positive-Integer])
+                                (format "    - ~aG Jug: ~a/~a"
+                                        cap (hash-ref st cap) cap))
+                              caps)
+                         "\n")))
 
   (define playing (j-node "Playing" #:type 'puzzle #:prompt (code prompt-playing)))
   (define check   (j-node "Check Clear" #:type 'check))
@@ -97,27 +102,41 @@
   (values
    (j-graph g
             #:edges
-            (list
-             (j-edge (format "Fill ~aG" left-cap) #:from playing #:to check #:when (code can-fill-left?) #:trans (code fill-left))
-             (j-edge (format "Fill ~aG" right-cap) #:from playing #:to check #:when (code can-fill-right?) #:trans (code fill-right))
-
-             (j-edge (format "Empty ~aG" left-cap) #:from playing #:to check #:when (code can-empty-left?) #:trans (code empty-left))
-             (j-edge (format "Empty ~aG" right-cap) #:from playing #:to check #:when (code can-empty-right?) #:trans (code empty-right))
-
-             (j-edge (format "Pour ~aG -> ~aG" left-cap right-cap) #:from playing #:to check #:when (code can-pour-left-to-right?) #:trans (code pour-left-to-right))
-             (j-edge (format "Pour ~aG -> ~aG" right-cap left-cap) #:from playing #:to check #:when (code can-pour-right-to-left?) #:trans (code pour-right-to-left))
-             (j-edge "Not yet" #:from check #:to playing #:mode 'auto #:when (code (negate is-cleared?)))
-             (j-edge "Clear!" #:from check #:to cleared #:mode 'auto #:when (code is-cleared?))))
+            `(,@(for/list : (Listof (Edge Jug-State))
+                          ([cap (in-list caps)])
+                  (j-edge (format "Fill ~aG" cap)
+                          #:from playing
+                          #:to check
+                          #:when (code (can-fill? cap))
+                          #:trans (code (fill cap))))
+              ,@(for/list : (Listof (Edge Jug-State))
+                          ([cap (in-list caps)])
+                  (j-edge (format "Empty ~aG" cap)
+                          #:from playing
+                          #:to check
+                          #:when (code (can-empty? cap))
+                          #:trans (code (empty cap))))
+              ,@(for*/list : (Listof (Edge Jug-State))
+                           ([cap1 (in-list caps)]
+                            [cap2 (in-list caps)]
+                            #:unless (= cap1 cap2))
+                  (j-edge (format "Pour ~aG -> ~aG" cap1 cap2)
+                          #:from playing
+                          #:to check
+                          #:when (code (can-pour? cap1 cap2))
+                          #:trans (code (pour cap1 cap2))))
+              ,(j-edge "Not yet" #:mode 'auto #:from check #:to playing #:when (code (negate is-cleared?)))
+              ,(j-edge "Clear!" #:mode 'auto #:from check #:to cleared #:when (code is-cleared?))))
    playing))
 
-(module+ model
-  (provide make-model)
-  (: make-model (-> Positive-Integer Positive-Integer Positive-Integer (Model Jug-State)))
-  (define (make-model left-cap right-cap target)
-    (model
-     (thunk
-      (define-values (graph node-init) (jug-graph "Water Jug Puzzle" left-cap right-cap target))
-      (values (list graph) node-init (jug-state 0 0))))))
+  (module+ model
+    (provide make-model)
+    (: make-model (-> (Listof Positive-Integer) Positive-Integer (Model Jug-State)))
+    (define (make-model caps target)
+      (model
+       (thunk
+        (define-values (graph node-init) (jug-graph "Water Jug Puzzle" caps target))
+        (values (list graph) node-init (caps->jug-state caps))))))
 
 (module+ main
   (require racket/cmdline
@@ -133,9 +152,11 @@
    [("--dot") "Generate dot" (set-box! mode 'dot)]
    [("--console") "Run console" (set-box! mode 'console)]
    #:args ()
-   (define m (make-model 3 5 4))
+   (define m (make-model '(3 5) 4))
    (case (unbox mode)
-     [(dot) (render-dot (dot-renderer m))]
+     [(dot) (render-dot (dot-renderer m)
+                        #:config (dot-config #:global
+                                             (dot-global-config #:rankdir 'LR)))]
      [(console)
       (parameterize ([current-console-commands (list (list 'quit 'q "Quit"))]
                      [current-console-trace-display 'hide])
@@ -144,7 +165,7 @@
 (module+ test
   (require typed/rackunit (submod ".." model))
 
-  (define m (make-model 3 5 4))
+  (define m (make-model '(3 5) 4))
 
   (: terminal-node? (-> (Node Jug-State) Boolean))
   (define (terminal-node? x) (eq? (node-type x) 'terminal))
@@ -153,14 +174,6 @@
   (check-false (find-deadlock m terminal-node?))
   (check-false (find-false-terminal m terminal-node?))
   (check-false (find-auto-conflict m))
-
-  (check-false (find-counterexample m (lambda (_n [st : Jug-State])
-                                        (<= 0 (jug-state-left st) 3))))
-  (check-false (find-counterexample m (lambda (_n [st : Jug-State])
-                                        (<= 0 (jug-state-right st) 5))))
-  (check-false (find-counterexample m (lambda (_n [st : Jug-State])
-                                        (<= (+ (jug-state-left st) (jug-state-right st))
-                                            (+ 3 5)))))
 
   (: shortest-path (-> (Model Jug-State) (Option Journal)))
   (define (shortest-path m)
@@ -181,4 +194,14 @@
                   (auto ("Not yet"))
                   (choose ("Pour 5G -> 3G"))
                   (auto ("Not yet"))
-                  (choose ("Fill 5G")))))
+                  (choose ("Fill 5G"))))
+  
+  (check-equal? (shortest-path (make-model '(3 5 7) 1))
+                '((auto ("Clear!"))
+                  (choose ("Pour 5G -> 7G"))
+                  (auto ("Not yet"))
+                  (choose ("Pour 3G -> 7G"))
+                  (auto ("Not yet"))
+                  (choose ("Fill 5G"))
+                  (auto ("Not yet"))
+                  (choose ("Fill 3G")))))
