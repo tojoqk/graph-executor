d4239838d19a841ece37039dcbc407c3de8ca9e9
Masaya Tojo <masaya@tojo.tokyo>
2026-08-28 22:33:57 +0900

    Export node-info? predicate from graph.rkt

--- graph.rkt
+++ graph.rkt
@@ -7,7 +7,7 @@
          Node node-maker
          node-graph-id node-graph-name node-id node-name node-type node-tags node-desc node-trans node-trans-code-expr node-before-code-expr node-after-code-expr node-prompt node-prompt-code-expr node-node-options
          Node-Option (struct-out node-option)
-         Node-Info node-node-info node-info-name node-info-type node-info-tags node-info-desc
+         Node-Info node-info? node-node-info node-info-name node-info-type node-info-tags node-info-desc
          any-node
          Edge Bridge EdgeMode edge? make-edge make-bridge
          edge-id edge-name edge-mode edge-half? edge-from edge-to edge-desc edge-when edge-when-code-expr edge-trans edge-trans-code-expr edge-before-code-expr edge-after-code-expr edge-priority edge-edge-options
