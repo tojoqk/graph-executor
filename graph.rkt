0b6a781ea97c554a3f8312fbe9c1fae8b738cb97
Masaya Tojo <masaya@tojo.tokyo>
2026-09-14 00:36:17 +0900

    Unexport node-graph-name and graph-name accessors

--- graph.rkt
+++ graph.rkt
@@ -3,12 +3,12 @@
 (require "private/graph.rkt")
 
 (provide Code code
-         Node node-graph-name
+         Node
          Node-Option
-         Node-Info node-node-info node-info-name node-info-type node-info-tags node-info-desc
+         Node-Info node-info-name node-info-type node-info-tags node-info-desc
          any-node
          Edge make-edge Bridge bridge? make-bridge
          Edge-Info edge-info-mode edge-info-name edge-info-desc edge-info-from edge-info-to
          Edge-Option
-         Graph graph-name graph-maker OpenGraph open-graph-maker
+         Graph graph-maker OpenGraph open-graph-maker
          any-graph)
